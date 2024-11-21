8b66bc02706dec098670cb6aa356770d107c3741
Jussi Pakkanen <jpakkane@gmail.com>
2024-11-21 20:20:47 +0200

    Dirty is as dirty does.

--- builder.py
+++ builder.py
@@ -27,16 +27,25 @@ class VS:
     def __init__(self):
         self.compcmd = ['cl']
         self.linkcmd = ['link']
+        self.vs_root = pathlib.WindowsPath(r'C:\Program Files\Microsoft Visual Studio\2022\Preview\VC\Tools\MSVC\14.43.34604')
 
     def compile(self, source, obj, use_istd, extra_args):
+        retval = None
         if use_istd:
-            sys.exit('Import std not supported for VS yet.')
-        cmd = self.compcmd + ['/nologo',
-                              '/c',
-                              f'/Fo{obj}',
-                              '/EHsc',
-                              source] + extra_args
+            retval = self.do_istd(source, obj, extra_args)
+            cmd = self.compcmd + ['/nologo',
+                                  '/c',
+                                  f'/Fo{obj}',
+                                  '/EHsc',
+                                  source] + extra_args
+        else:
+            cmd = self.compcmd + ['/nologo',
+                                  '/c',
+                                  f'/Fo{obj}',
+                                  '/EHsc',
+                                  source] + extra_args
         subprocess.check_call(cmd)
+        return retval
 
     def link(self, exename, objfiles):
         exename = exename.with_suffix('.exe')
@@ -45,6 +54,22 @@ class VS:
                               '/SUBSYSTEM:CONSOLE'] + objfiles
         subprocess.check_call(cmd)
 
+    def do_istd(self, source, obj, extra_args):
+        builddir = pathlib.WindowsPath(obj.parts[0])
+        stdobj = builddir / 'std.ixx.o'
+        if stdobj.exists():
+            return stdobj
+        ddi = builddir / 'std.ixx.obj.ddi'
+        cmd = self.compcmd + ['/nologo',
+                              '-TP',
+                              '/EHsc',
+#                              '-scanDependencies',
+#                              ddi,
+                              '/c',
+                              self.vs_root / 'modules/std.ixx',
+                              f'/Fo{stdobj}'] + extra_args
+        subprocess.check_call(cmd)
+        return stdobj
 
 class BuildSystem:
     def __init__(self, compiler, options):
@@ -54,21 +79,29 @@ class BuildSystem:
         self.builddir = pathlib.Path(options.sourcedir + '.b')
         self.use_istd = options.sourcedir.endswith('istd')
         self.exefile = self.builddir / 'program'
-        self.num_processes = None
+        self.num_processes = 1
 
     def build(self):
         self.builddir.mkdir(exist_ok=True)
         sources = self.srcdir.glob('*.cpp')
         objfiles = []
-        with ThreadPoolExecutor(self.num_processes) as tp:
-            futures = []
+        if self.num_processes == 1:
             for s in sources:
                 o = self.builddir / (s.parts[-1] + '.o')
-                h = tp.submit(self.compiler.compile, s, o, self.use_istd, self.extra_args)
+                stdo = self.compiler.compile(s, o, self.use_istd, self.extra_args)
+                if stdo is not None and stdo not in objfiles:
+                    objfiles.append(stdo)
                 objfiles.append(o)
-                futures.append(h)
-            for f in futures:
-                f.result()
+        else:
+            with ThreadPoolExecutor(self.num_processes) as tp:
+                futures = []
+                for s in sources:
+                    o = self.builddir / (s.parts[-1] + '.o')
+                    h = tp.submit(self.compiler.compile, s, o, self.use_istd, self.extra_args)
+                    objfiles.append(o)
+                    futures.append(h)
+                for f in futures:
+                    f.result()
         self.compiler.link(self.exefile, objfiles)
 
 
@@ -80,4 +113,3 @@ if __name__ == '__main__':
         compiler = GCC(['c++'], opts)
     build = BuildSystem(compiler, opts)
     build.build()
-
